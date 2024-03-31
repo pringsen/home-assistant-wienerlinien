66877cf2af35ec66dfe9e6eadee4a971989fcff9
pringsen <83004770+pringsen@users.noreply.github.com>
2024-03-31 14:19:18 +0200

    Update const.py to reflect next/following nomenclature

--- custom_components/wienerlinien/const.py
+++ custom_components/wienerlinien/const.py
@@ -2,6 +2,6 @@
 BASE_URL = "http://www.wienerlinien.at/ogd_realtime/monitor?rbl={}"
 
 DEPARTURES = {
-    "first": {"key": 0, "name": "{} first departure"},
-    "next": {"key": 1, "name": "{} next departure"},
+    "next": {"key": 0, "name": "{} next"},
+    "following": {"key": 1, "name": "{} following"},
 }
